e8cb197ffbeb0be8e1fbe6c152e63880830377ab
Nurdaulet <nurik9293709@gmail.com>
2025-04-16 03:23:32 +0500

    product card

--- src/app/(default)/catalog/components/products-carousel.tsx
+++ src/app/(default)/catalog/components/products-carousel.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
 import ArrowLeftIcon from "../../../components/ui/icons/arrow-left-icon";
-import StarRating from "./star-rating";
-import ProductPrice from "./product-price";
+import ProductCard from "./product-card";
 
 export default function ProductsCarousel({ title }: {
   title: String,
@@ -24,54 +22,20 @@ export default function ProductsCarousel({ title }: {
         </div>
       </div>
       <div className="flex gap-2 md:gap-5 px-4 md:px-5 scrollable-layout-container no-scrollbar">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </div>
-  );
-}
-
-const Product = () => {
-  return (
-    <Link
-      href="/catalog/uhod-za-licom/tonizaciya/tonik-skin-balancing-pore-reducing-toner-paulas-choice" className="min-w-40 md:min-w-58"
-    >
-      <div className="relative w-full h-49 md:h-76 bg-primary-muted">
-        <Image
-          src="/images/tmp/product.png"
-          alt="Product"
-          fill
-          className="object-contain mix-blend-multiply"
-        />
-      </div>
-      <div className="flex flex-col gap-2 py-3">
-        <div className="text-sm font-semibold">
-          Парфюмерная вода
-        </div>
-        <div className="text-lg leading-6">
-          Brazilian Crush Cheirosa 40 Perfume Mist
-        </div>
-        <div>
-          <div className="inline-block px-1.5 py-0.5 bg-accent rounded text-sm font-semibold">
-            350 Б
-          </div>
-        </div>
-        <div className="text-sm">
-          <ProductPrice />
-        </div>
-        <StarRating />
-      </div>
-    </Link>
   );
 }
 
