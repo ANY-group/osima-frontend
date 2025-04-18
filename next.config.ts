660b36d9082b31e210fc33fc312b63bdc6f35e55
Nurdaulet <nurik9293709@gmail.com>
2025-04-19 03:45:14 +0500

    image sizes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    deviceSizes: [320, 480, 640, 750, 768, 828, 1024, 1080, 1100, 1200, 1920, 2048, 3840],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3802',
+        pathname: '/storage/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
