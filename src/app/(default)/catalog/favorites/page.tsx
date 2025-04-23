d3bc9b4204b66a7941567b88e508ca05a892d34b
Nurdaulet <nurik9293709@gmail.com>
2025-04-24 03:04:54 +0500

    favorites insets

--- src/app/(default)/catalog/favorites/page.tsx
+++ src/app/(default)/catalog/favorites/page.tsx
@@ -4,14 +4,14 @@ import CatalogProductsGrid from "../components/catalog-products-grid";
 export default function FavoritePage() {
   return (
     <main>
-      <section className="sticky md:static top-18 bg-background max-md:border-b border-divider z-10">
+      <section className="sticky md:static top-18 bg-background border-divider z-10">
         <div className="layout-container">
           <Subheader title="Избранное">
             <span className="hidden md:inline">товаров 4</span>
           </Subheader>
         </div>
       </section>
-      <section className="layout-container mt-15">
+      <section className="layout-container md:mt-8">
         <CatalogProductsGrid />
       </section>
     </main>
