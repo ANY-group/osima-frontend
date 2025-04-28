27175fec4611d146b2b33274016418b837f365d6
Nurdaulet <nurik9293709@gmail.com>
2025-04-28 22:11:55 +0500

    cart add fav.

--- src/app/(default)/checkout/components/cart-item.tsx
+++ src/app/(default)/checkout/components/cart-item.tsx
@@ -5,17 +5,26 @@ import Image from "next/image";
 import Link from "@/app/components/ui/link";
 import ProductCashback from "../../catalog/components/product-cashback";
 import ProductPrice from "../../catalog/components/product-price";
+import HeartIcon from "@/app/components/ui/icons/heart-icon";
 
 export default function CartItem() {
   return (
     <div className="group flex gap-3 my-6 md:m-6 md:ml-2">
-      <Image
-        src="/images/tmp/product.png"
-        alt="Product"
-        width={100}
-        height={100}
-        className="object-contain w-25 h-25 aspect-square"
-      />
+      <div className="relative w-25 h-25 shrink-0 rounded-lg">
+        <Image
+          src="/images/tmp/product.png"
+          alt="Product"
+          width={100}
+          height={100}
+          className="object-contain h-full rounded-lg"
+        />
+        <button
+          className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center rounded-lg bg-foreground/40 transition-opacity opacity-0 group-hover:opacity-100 group-focus:opacity-100 text-danger"
+          aria-label="Добавить в избранные"
+        >
+          <HeartIcon />
+        </button>
+      </div>
       <div className="flex-grow">
         <div className="relative flex max-lg:flex-col items-start justify-between gap-3">
           <Link href="/catalog/category/subcategory/product" className="max-lg:mr-6">
---
+++ src/app/components/ui/icons/heart-icon.tsx
@@ -0,0 +1,5 @@
+export default function HeartIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.21 3.05a6.29 6.29 0 013.962 1.83 6.27 6.27 0 010 8.84l-6.21 6.22a2.78 2.78 0 01-3.9 0l-6.21-6.22a6.27 6.27 0 018.16-9.48 6.29 6.29 0 014.198-1.19z" fill="currentColor"></path></svg>
+  );
+}
