4cc2ccc7328225e5f517a548999977c8a41d12da
Nurdaulet <nurik9293709@gmail.com>
2025-05-21 13:07:13 +0500

    build fix

--- src/app/(default)/catalog/components/catalog-page.tsx
+++ src/app/(default)/catalog/components/catalog-page.tsx
@@ -1,32 +1,20 @@
 import fetchCatalog from "@/lib/catalog/usecases/fetch-catalog";
 import Catalog from "./catalog";
 
-export default async function CatalogPage({ params, searchParams }: {
+export default async function CatalogPage({ params }: {
   params: Promise<{
     categorySlug?: string,
     subcategorySlug?: string,
     brandSlug?: string,
   }>,
-  searchParams: Promise<{ [key: string]: any }>,
 }) {
   const { categorySlug, subcategorySlug, brandSlug } = await params;
-
-  const parsedFilters = Object.entries(await searchParams)
-    .reduce((acc, [key, value]) => {
-      const match = key.match(/^filters\[(.+)]$/);
-      if (match) {
-        const filterKey = match[1];
-        if (!acc.filters) acc.filters = {};
-        acc.filters[filterKey] = value;
-      }
-      return acc;
-    }, {} as any);
 
   const { category, subcategory, brand, products } = await fetchCatalog(
     categorySlug,
     subcategorySlug,
     brandSlug,
-    parsedFilters
+    {},
   );
 
   return (
