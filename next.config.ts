099bf2a508dea2ef38d2c425d0ce6b18d37b3714
Nurdaulet <nurik9293709@gmail.com>
2025-05-23 22:04:42 +0500

    image fix

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,13 @@ const nextConfig: NextConfig = {
     deviceSizes: [320, 480, 640, 750, 768, 828, 1024, 1080, 1100, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3802',
-        pathname: '/storage/**',
+        protocol: 'https',
+        hostname: 'admin.vegas.anygroup.kz',
+        pathname: '/**',
       },
       {
         protocol: 'http',
-        hostname: '127.0.0.1',
+        hostname: 'localhost',
         port: '3802',
         pathname: '/storage/**',
       },
