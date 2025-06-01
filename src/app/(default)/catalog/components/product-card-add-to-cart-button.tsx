242df1e23d2d80b5d94af3cf9337ee613c1561b7
Nurdaulet <nurik9293709@gmail.com>
2025-06-02 03:03:22 +0500

    outof stock button color

--- src/app/(default)/catalog/components/product-card-add-to-cart-button.tsx
+++ src/app/(default)/catalog/components/product-card-add-to-cart-button.tsx
@@ -19,12 +19,14 @@ export default function ProductCardAddToCartButton({ product }: {
       <button
         className="flex items-center justify-center gap-1
         px-2 md:px-3 py-1 md:py-2 md:mx-auto md:w-full
-        rounded bg-accent hover:not-disabled:bg-success disabled:bg-disabled/40
+        rounded bg-accent hover:not-disabled:bg-success disabled:bg-transparent disabled:text-secondary
         text-sm font-semibold whitespace-nowrap transition-colors"
         onClick={() => setItemQuantity(product, 1)}
         disabled={!product.inStock}
       >
-        <ShoppingBagIcon />
+        {product.inStock && (
+          <ShoppingBagIcon />
+        )}
         {product.inStock ? 'Добавить' : 'Нет в наличии'}
       </button>
     );
