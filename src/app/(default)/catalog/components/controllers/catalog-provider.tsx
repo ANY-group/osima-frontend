1215d68604f0101449082f7f34a05861ccb57291
Nurdaulet <nurik9293709@gmail.com>
2025-06-01 06:54:16 +0500

    scroll fix

--- src/app/(default)/catalog/components/controllers/catalog-provider.tsx
+++ src/app/(default)/catalog/components/controllers/catalog-provider.tsx
@@ -50,7 +50,7 @@ export default function CatalogProvider({
       }
     }
 
-    replace(`${pathname}?${params.toString().replace(/%2C/g, ',')}`);
+    replace(`${pathname}?${params.toString().replace(/%2C/g, ',')}`, { scroll: false });
   }, [pathname, filtersMap]);
 
   const isFilterApplied = (filter: FilterEntity, value?: FilterValueEntity) => {
