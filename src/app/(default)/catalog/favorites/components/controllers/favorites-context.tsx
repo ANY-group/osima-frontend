cbd5d8d7e714216f221ba7151cafd7765289545f
Nurdaulet <nurik9293709@gmail.com>
2025-06-02 01:27:29 +0500

    lint fixes

--- src/app/(default)/catalog/favorites/components/controllers/favorites-context.tsx
+++ src/app/(default)/catalog/favorites/components/controllers/favorites-context.tsx
@@ -9,6 +9,6 @@ export const FavoritesContext = createContext<{
   favoriteIds: Array<number>,
 }>({
   isFavorite: () => false,
-  toggleFavorite: (product: ProductEntity) => { },
+  toggleFavorite: () => { },
   favoriteIds: [],
 });
