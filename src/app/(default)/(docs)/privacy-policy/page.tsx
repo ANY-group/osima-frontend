ac6c4ddd46e1f797a4198f3f951640a8e213ad09
Nurdaulet <nurik9293709@gmail.com>
2025-06-06 04:45:28 +0500

    fixes

--- src/app/(default)/(docs)/privacy-policy/page.tsx
+++ src/app/(default)/(docs)/privacy-policy/page.tsx
@@ -6,20 +6,18 @@ export default async function PrivacyPolicyPage() {
   const page = await fetchPage<PrivacyPolicyPageEntity>('privacy-policy');
 
   return (
-    <main>
-      <section className="layout-container">
-        <Breadcrumbs items={[
-          {
-            label: 'Документы',
-            href: null,
-          },
-          {
-            label: 'Политика конфиденциальности',
-            href: null,
-          },
-        ]} />
-      </section>
-      <section className="layout-container-sm my-10">
+    <main className="layout-container">
+      <Breadcrumbs items={[
+        {
+          label: 'Документы',
+          href: null,
+        },
+        {
+          label: 'Политика конфиденциальности',
+          href: null,
+        },
+      ]} />
+      <section className="my-10">
         <div className="raw-content" dangerouslySetInnerHTML={{ __html: page.content || '' }}>
         </div>
       </section>
--- src/app/(default)/(docs)/public-offer/page.tsx
+++ src/app/(default)/(docs)/public-offer/page.tsx
@@ -6,21 +6,19 @@ export default async function PublicOfferPage() {
   const page = await fetchPage<PublicOfferPageEntity>('public-offer');
 
   return (
-    <main>
-      <section className="layout-container">
-        <Breadcrumbs items={[
-          {
-            label: 'Документы',
-            href: null,
-          },
-          {
-            label: 'Публичная оферта',
-            href: null,
-          },
-        ]} />
-      </section>
+    <main className="layout-container">
+      <Breadcrumbs items={[
+        {
+          label: 'Документы',
+          href: null,
+        },
+        {
+          label: 'Публичная оферта',
+          href: null,
+        },
+      ]} />
 
-      <section className="layout-container-sm my-10">
+      <section className="my-10">
         <div className="raw-content" dangerouslySetInnerHTML={{ __html: page.content || '' }}>
         </div>
       </section>
