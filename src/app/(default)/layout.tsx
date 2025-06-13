8eaf550970b074e8a1765b9b2a3182bf885d9e63
Nurdaulet <nurik9293709@gmail.com>
2025-06-14 04:11:19 +0500

    categories fix

--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -1,22 +1,16 @@
-import fetchCategories from "@/lib/catalog/usecases/fetch-categories";
 import Footer from "../components/footer";
 import Header from "../components/header";
-import CategoriesProvider from "./catalog/components/controllers/categories-provider";
 
-export default async function DefaultLayout({
+export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode,
 }) {
-  const categories = await fetchCategories();
-
   return (
     <div className="min-h-lvh">
-      <CategoriesProvider categories={categories}>
-        <Header />
-        {children}
-        <Footer />
-      </CategoriesProvider>
+      <Header />
+      {children}
+      <Footer />
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import AuthProvider from "./(footerless)/profile/components/controllers/auth-pro
 import fetchUser from "@/lib/auth/usecases/fetch-user";
 import CartProvider from "./(default)/checkout/components/controllers/cart-provider";
 import FavoritesProvider from "./(default)/catalog/favorites/components/controllers/favorites-provider";
+import CategoriesProvider from "./(default)/catalog/components/controllers/categories-provider";
+import fetchCategories from "@/lib/catalog/usecases/fetch-categories";
 
 const roboto = Roboto_Flex({
   subsets: ["latin", "cyrillic"],
@@ -22,19 +24,24 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = await fetchUser()
-    .catch(() => null);
+  const [user, categories] = await Promise.all([
+    await fetchUser().catch(() => null),
+    fetchCategories(),
+  ]);
+
 
   return (
     <html lang="ru" className="md:bg-foreground scroll-smooth">
       <body className={`antialiased ${roboto.className}`}>
-        <AuthProvider initialUser={user}>
-          <FavoritesProvider>
-            <CartProvider>
-              {children}
-            </CartProvider>
-          </FavoritesProvider>
-        </AuthProvider>
+        <CategoriesProvider categories={categories}>
+          <AuthProvider initialUser={user}>
+            <FavoritesProvider>
+              <CartProvider>
+                {children}
+              </CartProvider>
+            </FavoritesProvider>
+          </AuthProvider>
+        </CategoriesProvider>
         <Loader />
       </body>
     </html>
